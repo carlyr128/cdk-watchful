fb359bc5052e9f18646a46c1825f126072a039ba
Richardson, Carly <carly.richardson@pearson.com>
2021-12-16 21:41:56 -0500

    removing double quotes

--- example/index.ts
+++ example/index.ts
@@ -1,57 +1,49 @@
-import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
-import * as events from "aws-cdk-lib/aws-events";
-import * as events_targets from "aws-cdk-lib/aws-events-targets";
-import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as path from "path";
-import * as sns from "aws-cdk-lib/aws-sns";
-import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as events_targets from 'aws-cdk-lib/aws-events-targets';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-import { App, Duration, Stack, StackProps } from "aws-cdk-lib";
+import { App, Duration, Stack, StackProps } from 'aws-cdk-lib';
 
-import { Construct } from "constructs";
-import { Watchful } from "../src";
+import { Construct } from 'constructs';
+import { Watchful } from '../src';
 
 class TestStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    const table1 = new dynamodb.Table(this, "DynamoTable1", {
+    const table1 = new dynamodb.Table(this, 'DynamoTable1', {
       writeCapacity: 10,
       partitionKey: {
-        name: "ID",
+        name: 'ID',
         type: dynamodb.AttributeType.STRING,
       },
     });
 
-    const writeTraffic = new TrafficDriver(this, "WriteTraffic", {
+    const writeTraffic = new TrafficDriver(this, 'WriteTraffic', {
       table: table1,
       write: true,
     });
 
-    const readTraffic = new TrafficDriver(this, "WriteReadTraffic", {
+    const readTraffic = new TrafficDriver(this, 'WriteReadTraffic', {
       table: table1,
       write: true,
       read: true,
     });
 
-    const alarmSqs = sqs.Queue.fromQueueArn(
-      this,
-      "AlarmQueue",
-      "arn:aws:sqs:us-east-1:444455556666:alarm-queue"
-    );
-    const alarmSns = sns.Topic.fromTopicArn(
-      this,
-      "AlarmTopic",
-      "arn:aws:sns:us-east-2:444455556666:MyTopic"
-    );
+    const alarmSqs = sqs.Queue.fromQueueArn(this, 'AlarmQueue', 'arn:aws:sqs:us-east-1:444455556666:alarm-queue');
+    const alarmSns = sns.Topic.fromTopicArn(this, 'AlarmTopic', 'arn:aws:sns:us-east-2:444455556666:MyTopic');
 
-    const watchful = new Watchful(this, "watchful", {
-      alarmEmail: "benisrae@amazon.com",
+    const watchful = new Watchful(this, 'watchful', {
+      alarmEmail: 'benisrae@amazon.com',
       alarmSqs,
       alarmSns,
     });
 
-    watchful.watchDynamoTable("My Cute Little Table", table1);
+    watchful.watchDynamoTable('My Cute Little Table', table1);
 
     watchful.watchScope(writeTraffic);
     watchful.watchScope(readTraffic);
@@ -74,14 +66,14 @@ class TrafficDriver extends Construct {
       throw new Error('At least "read" or "write" must be set');
     }
 
-    this.fn = new lambda.Function(this, "LambdaFunction", {
-      code: lambda.Code.asset(path.join(__dirname, "lambda")),
+    this.fn = new lambda.Function(this, 'LambdaFunction', {
+      code: lambda.Code.asset(path.join(__dirname, 'lambda')),
       runtime: lambda.Runtime.NODEJS_10_X,
-      handler: "index.handler",
+      handler: 'index.handler',
       environment: {
         TABLE_NAME: props.table.tableName,
-        READ: props.read ? "TRUE" : "",
-        WRITE: props.write ? "TRUE" : "",
+        READ: props.read ? 'TRUE' : '',
+        WRITE: props.write ? 'TRUE' : '',
       },
     });
 
@@ -93,7 +85,7 @@ class TrafficDriver extends Construct {
       props.table.grantReadData(this.fn);
     }
 
-    new events.Rule(this, "Tick", {
+    new events.Rule(this, 'Tick', {
       schedule: events.Schedule.rate(Duration.minutes(1)),
       targets: [new events_targets.LambdaFunction(this.fn)],
     });
@@ -104,7 +96,7 @@ class TestApp extends App {
   constructor() {
     super();
 
-    new TestStack(this, "watchful-example");
+    new TestStack(this, 'watchful-example');
   }
 }
 
